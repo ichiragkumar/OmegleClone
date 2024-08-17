66f07cfb8011076118594fa6f0d6b9bf4d3d2c56
CHIRAG KUMAR <chiragroy3030@gmail.com>
2024-08-17 10:35:54 +0530

    added query params for individual joining

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,3 +1,14 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
 export const LandingPage = () =>{
-    return <div>Landing Page </div>
+    const [name, setName ] = useState("")
+    return <div>
+
+        <input type="text" onChange={(e)=>{
+            setName(e.target.value)
+        }} placeholder="Enter your Name" />
+
+        <Link to={`/room/?name=${name}`}> Join Now
+        </Link>
+    </div>
 }
--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,5 +1,23 @@
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom"
+
+
 export const Room  = () =>{
+    const [searchParams , setSearchParms] = useSearchParams()
+    const name = searchParams.get("name");
+
+
+
+    // Logic to Let user init to the room 
+    useEffect(()=>{
+
+
+
+    }, [name])
+  
+
+    
     return <div>
-        Room Page
+        hii  {name}
     </div>
 }
